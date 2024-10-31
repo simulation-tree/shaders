bd893bcbc602360f1d1fcb1551dc385beee6d429
Phill <philldamaskin@gmail.com>
2024-10-30 21:08:38 -0400

    clean up after split in unmanaged

--- source/ShaderUniformProperty.cs
+++ source/ShaderUniformProperty.cs
@@ -32,7 +32,7 @@ namespace Shaders
 
         public unsafe readonly override string ToString()
         {
-            USpan<char> buffer = stackalloc char[(int)(FixedString.MaxLength + 16)];
+            USpan<char> buffer = stackalloc char[(int)(FixedString.Capacity + 16)];
             uint length = ToString(buffer);
             return buffer.Slice(0, length).ToString();
         }
