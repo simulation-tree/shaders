0c16e25642246593323c07dc819fb0451b8d201f
Phill <philldamaskin@gmail.com>
2025-01-25 16:51:19 -0500

    Update ShaderTests.cs

--- tests/ShaderTests.cs
+++ tests/ShaderTests.cs
@@ -9,14 +9,14 @@ namespace Shaders.Tests
         static ShaderTests()
         {
             TypeRegistry.Load<Shaders.TypeBank>();
-            TypeRegistry.Load<Data.TypeBank>();
+            TypeRegistry.Load<Data.Core.TypeBank>();
         }
 
         protected override Schema CreateSchema()
         {
             Schema schema = base.CreateSchema();
             schema.Load<Shaders.SchemaBank>();
-            schema.Load<Data.SchemaBank>();
+            schema.Load<Data.Core.SchemaBank>();
             return schema;
         }
     }
